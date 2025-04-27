4a2397509866257bcc6ddccda28fd683192dda6e
Влад Сергеев <vladsergeev@MacBook-Air-Vlad.local>
2025-04-27 11:45:54 -0700

    Новые изменения

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ class FindNumberGame {
         this.levelDisplay = document.getElementById('level');
         this.scoreDisplay = document.getElementById('score');
         this.bonusDisplay = document.getElementById('bonus');
+        this.tableNumbers = document.getElementById('numbers');
+    }
+
+    generateNumbers() {
+
+    }
+    
+    startGame() {
+
     }
 }
 
