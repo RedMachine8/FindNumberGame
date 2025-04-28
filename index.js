60a864c55e4b50b6d261c8741bc69f73b4660b76
RedMachine8 <redmachine8@yandex.ru>
2025-04-28 18:07:35 +0300

    Добавление логики и изменение стилей

--- index.js
+++ index.js
@@ -5,15 +5,69 @@ class FindNumberGame {
         this.scoreDisplay = document.getElementById('score');
         this.bonusDisplay = document.getElementById('bonus');
         this.tableNumbers = document.getElementById('numbers');
+        this.numbers = [];
+        this.targetNumber = document.getElementById('target-number');
     }
 
     generateNumbers() {
+        this.targetNumber.textContent = Math.floor(Math.random() * 999) + 1;
+        let number = 0;
+        for(let i = 0; i<8; i++) {
+            this.numbers.push(Math.floor(Math.random() * 999) + 1);
+        }
+        this.numbers.push(this.targetNumber.textContent);
+        this.numbers = this.shuffleArray(this.numbers);
+    }
 
+    shuffleArray(array) {
+        for(let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    createGrid() {
+        this.tableNumbers.innerHTML = '';
+        this.numbers.forEach(num => {
+             const cell = document.createElement('div');
+             cell.textContent = num;
+             cell.className = 'number';
+             cell.addEventListener('click', () => this.checkNumber(num));
+             this.tableNumbers.appendChild(cell);
+        });
+    }
+
+    checkNumber(clickedNum) {
+        if(clickedNum === this.targetNumber.textContent) {
+            this.levelDisplay.textContent = parseInt(this.levelDisplay.textContent) + 1;
+            this.bonusDisplay.textContent = parseInt(this.bonusDisplay.textContent) + 1;
+            this.scoreDisplay.textContent += parseInt(this.scoreDisplay.textContent) +  (15 * parseInt(this.bonusDisplay.textContent));
+            this.nextLevel(); 
+        } else {
+            this.levelDisplay.textContent = parseInt(this.levelDisplay.textContent) + 1;
+            this.bonusDisplay.textContent = parseInt(this.bonusDisplay.textContent) - 1;
+            this.scoreDisplay.textContent = parseInt(this.scoreDisplay.textContent) - 15;
+            this.nextLevel();
+        }
     }
     
-    startGame() {
+    nextLevel() {
+        this.numbers = [];
+        this.generateNumbers();
+        this.createGrid();
+    }
 
+    startGame() {
+        this.levelDisplay.textContent = 1;
+        this.bonusDisplay.textContent = 1;
+        this.scoreDisplay.textContent = 0;
+        this.generateNumbers();
+        this.createGrid();
     }
 }
 
-new FindNumberGame();
+const newGame = new FindNumberGame();
+window.addEventListener('load', () => {
+    newGame.startGame();
+});
